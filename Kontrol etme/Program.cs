f1074aaf09518ed24500b1fd94a23bf2895bc2b1
Nihat BEYI <nihatbeyi@hotmail.com>
2022-12-12 01:05:29 +0300

    Ornek calismam

---
+++ Kontrol etme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kontrol_etme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string ogrno, ad, soyad;
+            basadon:
+            Console.Write("Lütfen Ögrenci numarasını Giriniz..:");
+            ogrno = Console.ReadLine();
+            Console.Write("Lüfen Adınızı Giriniz..:");
+            ad = Console.ReadLine();
+            Console.Write("Lütfen Soyadınızı Giriniz..:");
+            soyad = Console.ReadLine();
+
+            if (string.IsNullOrEmpty(ogrno) == true || string.IsNullOrEmpty(ad) || string.IsNullOrEmpty(soyad) == true)
+            {
+                Console.WriteLine("Boş Gecilmıs demektir..");
+                goto basadon;
+            }
+            else
+            {
+                Console.WriteLine("Kayıt işlemi yapabilriniz..(dolu Demektir..)");//bos degil
+                goto basadon;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ String Türleri/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Türleri
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tc = 1234567891;
+            string adi = "Nihat";
+            string Soyad = "Beyi";
+            double maas = 8.500;
+
+            string birlestir = string.Concat("Tc Numarası: " + tc + " Adı:" + adi + " Soyadı:" + Soyad + " Maaş:" + maas);
+            Console.WriteLine(birlestir);
+
+            //1.ornek
+            int karsilastirma = String.Compare("admin", "admin");
+            Console.WriteLine("Birbirinden aynı:{0}", karsilastirma);
+
+            int karsılastir = string.Compare("admin", "Admin");
+            Console.WriteLine("Birbirinden farklı:{0}", karsılastir);
+
+            Console.Write("Lütfen kullanıcı adınızı giriniz..: ");
+            string ad = Console.ReadLine();
+        tekrarsifre:
+            Console.Write("Lütfen şifrenizi giriniz..: ");
+            string sifre1 = Console.ReadLine();
+            Console.Write("Lütfen şifrenizi tekrardan giriniz..: ");
+            string sifre2 = Console.ReadLine();
+
+            int kontrol = string.Compare(sifre1, sifre2);
+            if (kontrol == 0)
+            {
+                string sifre = sifre2;
+                Console.WriteLine("Kullanıcı adı:{0} şifre:{1}", ad, sifre, true);//buyuk kucuk harf duyarlılık
+                Console.WriteLine("şifreniz başarılı");
+            }
+            else if (kontrol == -1)
+            {
+                Console.WriteLine("şifreniz birbiriyle uyuşmuyor");
+                goto tekrarsifre;
+            }
+            else
+            {
+                //sifre 1 olması durumunda
+                Console.WriteLine("Şifreniz birbiriyle uyuşmuyor...");
+                goto tekrarsifre;
+            }
+            Console.ReadKey();
+        }
+    }
+}
